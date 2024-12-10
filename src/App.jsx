9da20ca39a1233d3e64730189ea56c32134cd43a
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2024-12-10 19:47:54 +0600

    Form Customized

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,14 @@ import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPageSing , {jobLoader} from './pages/JobPageSing';
+import AddJobPage from './pages/AddJobPage';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<MainLayout/>}>
             < Route index element={<HomePage/>} />
             < Route path='/jobs' element={<JobsPage/>} />
+            < Route path='/add-job' element={<AddJobPage/>} />
             < Route path='/jobs/:id' element={<JobPageSing/>} loader={jobLoader} />
             < Route path='*' element={<NotFoundPage/>} />
         </Route>
