ea4e6e19c13a58084c6359bd240f72b3ed469fce
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2025-03-01 20:16:47 +0600

    Router Setup Completed

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import Hero from '../components/Hero'
+import HomeCard from '../components/HomeCard'
+import JobListings from '../components/JobListings'
+import ViewAllJobs from '../components/ViewAllJobs'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+    <Hero/>
+    <HomeCard/>
+    <JobListings/>
+    <ViewAllJobs/>
+    </>
+  )
+}
+
+export default HomePage
