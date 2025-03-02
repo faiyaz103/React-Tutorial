313ce9e4cb98687526c9b8b8bd7837d40ba05cb1
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2025-03-02 14:26:18 +0600

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ const router=createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<MainLayout/>}>
             <Route index element={<HomePage/>}/>
+            <Route path='/React-Tutorial' element={<HomePage/>}/>
             <Route path='/jobs' element={<JobsPage/>}/>
             <Route path='*' element={<ErrorPage/>}/>
         </Route>
